5c32c44853801b682062f5da5d01a98518cc4b7f
Xavier Garcia <xavicogarcia12@gmail.com>
2021-06-24 06:57:50 -0500

    Barra de busqueda inicio

--- now-ui-kit-angular-master/src/app/components/inicio/inicio.component.ts
+++ now-ui-kit-angular-master/src/app/components/inicio/inicio.component.ts
@@ -7,35 +7,24 @@ import { NgbAccordionConfig, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
-  data : Date = new Date();
 
-  page = 4;
-  page1 = 5;
-  page2 = 3;
-  focus;
-  focus1;
-  focus2;
+  establecimientos: any[] = [
+      {
+        tipo: "Parques",
+        img: "assets/img/soccer-field.png"
+      },
+      {
+        tipo: "Complejos Deportivos",
+        img: "assets/img/estadio.png"
+      },
+      {
+        tipo: "Clubes",
+        img: "assets/img/club.png"
+      }
+  ]
 
-  date: {year: number, month: number};
-  model: NgbDateStruct;
+  constructor() {
 
-  public isCollapsed = true;
-  public isCollapsed1 = true;
-  public isCollapsed2 = true;
-
-  state_icon_primary = true;
-
-  constructor( private renderer : Renderer2, config: NgbAccordionConfig) {
-      config.closeOthers = true;
-      config.type = 'info';
-  }
-  isWeekend(date: NgbDateStruct) {
-      const d = new Date(date.year, date.month - 1, date.day);
-      return d.getDay() === 0 || d.getDay() === 6;
-  }
-
-  isDisabled(date: NgbDateStruct, current: {month: number}) {
-      return date.month !== current.month;
   }
 
   ngOnInit() {
