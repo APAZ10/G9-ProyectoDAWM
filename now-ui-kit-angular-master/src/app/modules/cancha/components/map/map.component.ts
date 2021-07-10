810a7a430dd8d29559dde7cf325467de5f0858b1
Alejandro Paz <alejandro_paz98@hotmail.com>
2021-07-10 14:30:14 -0500

    agregado mapa con coordenadas. Falta arreglar el marker en mapa.component

--- now-ui-kit-angular-master/src/app/modules/cancha/components/map/map.component.ts
+++ now-ui-kit-angular-master/src/app/modules/cancha/components/map/map.component.ts
@@ -1,6 +1,23 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 
+const iconRetinaUrl = 'assets/img/marker-icon-2x.png';
+const iconUrl = 'assets/img/marker-icon.png';
+const shadowUrl = 'assets/img/marker-shadow.png';
+//const shadowUrl = '../../../../../assets/img/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
+
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -12,17 +29,45 @@ export class MapComponent implements OnInit, AfterViewInit {
   private map;
 
   private initMap(): void {
-    this.map = L.map('map', {
-      center: [ 39.8282, -98.5795 ],
-      zoom: 3
-    });
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 3,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-
-    tiles.addTo(this.map);
+    fetch("../../../assets/data/canchas.json")
+    .then(response => response.json())
+    .then(data => {
+      let listaCanchas = data.canchas;
+      for(let cancha of listaCanchas as any){
+        if(cancha.nombre==="Cancha de fútbol: La Fortaleza"){
+          const lon = cancha.coordenadas[0];
+          const lat = cancha.coordenadas[1];
+          this.map = L.map('map', {
+            center: [ lon, lat ],
+            zoom: 18
+          });
+          const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            minZoom: 14,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+          });
+          tiles.addTo(this.map);
+          const marker = L.marker([lat, lon]);
+          const iconRetinaUrl = 'assets/img/marker-icon-2x.png';
+          const iconUrl = 'assets/img/marker-icon.png';
+          const shadowUrl = 'assets/img/marker-shadow.png';
+          const iconDefault = L.icon({
+            iconRetinaUrl,
+            iconUrl,
+            shadowUrl,
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            tooltipAnchor: [16, -28],
+            shadowSize: [41, 41]
+          });
+          marker.options.icon = iconDefault;
+          console.log(marker);
+          marker.addTo(this.map);
+        }
+      }
+    })
+    .catch(console.error);
   }
 
   constructor() { }
